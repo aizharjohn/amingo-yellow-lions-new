c00c8a53e87689fedc7f820c2a3fe5ddd13c306f
Aizhar John Quindoza <aizhar.quindoza@gmail.com>
2020-02-23 18:49:42 +0400

    Revised the Feed Load Feed and Topic files

--- frontend/src/components/FeedPage.js
+++ frontend/src/components/FeedPage.js
@@ -1,67 +1,58 @@
 import React, { Fragment } from 'react';
-import Navbar from './layout/Navbar';
 import CardLayout from './CardLayout';
 import Card from './Card';
 import LoadFeedButton from './LoadFeedButton';
 
-
 const FeedPage = () => {
- 
-    return (
-      
-      <div>
-
-      <Navbar />
+  return (
+    <div>
       <section className="jumbotron text-center">
         <div className="container">
           <h1 className="jumbotron-heading">Feeds</h1>
-          <p className="lead text-muted">You must be logged in to view the feeds. Please click the button to load the feeds.</p>
+          <p className="lead text-muted">
+            You must be logged in to view the feeds. Please click the button to
+            load the feeds.
+          </p>
         </div>
       </section>
-    <center>
-    <LoadFeedButton />
-    </center>
+      <center>
+        <LoadFeedButton />
+      </center>
 
       <CardLayout>
-                <Card
-                  image="https://cdn.pixabay.com/photo/2018/09/23/09/15/smoothie-3696961_960_720.jpg"
-                  title="Diabetes"
-                  description="Looking for help and support managing diabetes?"
-                />
-                <Card
-                  image="https://cdn.pixabay.com/photo/2018/08/09/02/49/person-3593657_960_720.jpg"
-                  title="Cardiology"
-                  description="Find others with cardiology issues and support each other"
-                />
-                <Card
-                  image="https://cdn.pixabay.com/photo/2018/08/13/03/21/woman-3602245_960_720.jpg"
-                  title="Expecting Parents"
-                  description="Meet other parents and attend workshops together"
-                />
-                <Card
-                  image="https://images.unsplash.com/photo-1466193341027-56e68017ee2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                  title="Cancer Support"
-                  description="Find others who are going through and work together"
-                />
-                <Card
-                  image="https://images.unsplash.com/photo-1555819206-7b30da4f1506?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
-                  title="Kidney Disease"
-                  description="Find friends and attend dialysis sessions together"
-                />
-                <Card
-                  image="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                  title="Weight Management"
-                  description="Need help in managing your weight, tips on diets?"
-                />
-              </CardLayout>
-     
-
-      </div>
-
-
-
-
-    );
-  };
+        <Card
+          image="https://cdn.pixabay.com/photo/2018/09/23/09/15/smoothie-3696961_960_720.jpg"
+          title="Diabetes"
+          description="Looking for help and support managing diabetes?"
+        />
+        <Card
+          image="https://cdn.pixabay.com/photo/2018/08/09/02/49/person-3593657_960_720.jpg"
+          title="Cardiology"
+          description="Find others with cardiology issues and support each other"
+        />
+        <Card
+          image="https://cdn.pixabay.com/photo/2018/08/13/03/21/woman-3602245_960_720.jpg"
+          title="Expecting Parents"
+          description="Meet other parents and attend workshops together"
+        />
+        <Card
+          image="https://images.unsplash.com/photo-1466193341027-56e68017ee2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+          title="Cancer Support"
+          description="Find others who are going through and work together"
+        />
+        <Card
+          image="https://images.unsplash.com/photo-1555819206-7b30da4f1506?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+          title="Kidney Disease"
+          description="Find friends and attend dialysis sessions together"
+        />
+        <Card
+          image="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+          title="Weight Management"
+          description="Need help in managing your weight, tips on diets?"
+        />
+      </CardLayout>
+    </div>
+  );
+};
 
 export default FeedPage;
--- frontend/src/components/LoadFeedButton.js
+++ frontend/src/components/LoadFeedButton.js
@@ -4,55 +4,46 @@ import CardLayout from './CardLayout';
 import Card from './Card.js';
 
 const LoadFeedButton = () => {
+  // global state
+  //    const [globalState, setGlobalState] = useContext(
+  //        AppContext
+  //    );
 
-    // global state
-//    const [globalState, setGlobalState] = useContext(
-//        AppContext
-//    );
+  // local state
+  const [state, setState] = useState({ feed: [] });
 
-    // local state
-    const [state, setState] = useState(
-        { feed: [] }
-    )
+  const loadFeed = () => {
+    // Fetch request goes
+    fetch('http://localhost:5000/feed/all')
+      .then(response => response.json())
+      .then(json => {
+        setState({
+          ...state,
+          feed: json.results
+        });
+      });
+  };
 
-    const loadFeed = () => {
-        // Fetch request goes
-        fetch('http://localhost:5000/feed/all')
-        .then(response => response.json())
-        .then(json=> {
-            setState(
-                {
-                    ...state,
-                    feed: json.results
-                }
-            )
-        })
-        
-    }
-
- if(state.feed.length === 0) {
-        return (
-            <button onClick={loadFeed}>Load Feed</button>
-        )
-    } else {
-        return (
-
-            <CardLayout>
-            {
-                state.feed.map(
-                    (item)=>
-                    <Card 
-                        title={``}
-                        image={item.image}
-                        description={item.description}
-                        buttonLabel="View Post"
-                    />
-                )
-            }
-            </CardLayout>
-
-        )
-    }
-}
+  if (state.feed.length === 0) {
+    return (
+      <button className="btn btn-primary my-1" onClick={loadFeed}>
+        Load Feed
+      </button>
+    );
+  } else {
+    return (
+      <CardLayout>
+        {state.feed.map(item => (
+          <Card
+            title={``}
+            image={item.image}
+            description={item.description}
+            buttonLabel="View Post"
+          />
+        ))}
+      </CardLayout>
+    );
+  }
+};
 
 export default LoadFeedButton;
